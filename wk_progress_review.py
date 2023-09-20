3c6b7cb233e6f26c5b0b31d4a6e9aa0d1f7de707
amcclead7336 <n7q4c1@u.northwestern.edu>
2023-09-19 23:38:10 -0500

    Add basic data handling and storing

--- wk_progress_review.py
+++ wk_progress_review.py
@@ -1,7 +1,68 @@
 import requests
-apikey_filename = "wk_apikey.txt"
+import os
+import sys
+import json
+import datetime
+from pprint import pprint
 
-with open(apikey_filename, "r") as f:
-    apikey = f.read().strip()
+WK_URL = "https://api.wanikani.com/v2/"
+AK_FILENAME = "wk_apikey.txt"
+datetime_string_format = "%Y-%m-%dT%H:%M:%S.%fZ"
+VERBOSE = True
+REFRESH = False
 
-print(apikey)
+def vprint(string):
+    if VERBOSE:
+        print(string)
+
+
+def create_store():
+    if "Store" not in os.listdir():
+        os.mkdir("Store")
+        vprint("Created Store Dir")
+    else:
+        vprint("Store Exists")
+
+
+def collect_data(endpoint, AUTH_HEADER):
+    endpoint_path = f"Store/{endpoint}.json"
+    check_flag = os.path.exists(endpoint_path)
+
+    if not check_flag or REFRESH:
+        vprint(f"Collecting Data: {endpoint}")
+        req = requests.get(f"{WK_URL}/{endpoint}", headers=AUTH_HEADER)
+        if req.status_code == 200:
+            with open(endpoint_path, "w") as f:
+                f.write(json.dumps(req.json(),indent=2))
+
+            level_data = req.json()
+        else:
+            print(req)
+            print("Error collecting data")
+            sys.exit(1)
+            
+    else:
+        vprint(f"Loading File: {endpoint_path}")
+        with open(endpoint_path, "r") as f:
+            level_data = json.load(f)
+
+    return level_data
+
+
+def main():
+
+    with open(AK_FILENAME, "r") as f:
+        apikey = f.read().strip()
+
+    AUTH_HEADER = {"Authorization":f"Bearer {apikey}"}
+
+    create_store()
+
+    level_data = collect_data("level_progressions", AUTH_HEADER)
+    
+    # vprint(req)
+    pprint(level_data)
+
+
+if __name__ == "__main__":
+    main()
