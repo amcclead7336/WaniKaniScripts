16e3a5e91a6c5b895419ebc53dae5e04dcb03dbd
amcclead7336 <n7q4c1@u.northwestern.edu>
2023-09-23 11:46:54 -0500

    POC Done, Add collect_progress_data and graph

---
+++ app.py
@@ -0,0 +1,107 @@
+from dash import Dash
+from dash import dcc
+from dash import html
+import pandas as pd
+
+level_data = pd.read_csv("Data/level_data.csv")
+
+progress_data = pd.read_csv("Data/progress_data.csv")
+graph_data = []
+
+for i,subject_type in enumerate(("radical","kanji","vocab")):
+    fig_data = []
+
+    for time in ("Unlock_times","Start_times","Passed_times"):
+        tmp_data = progress_data.query(f"Subject == '{subject_type}' and Des == '{time}'")
+        fig_data.append({
+            "x": tmp_data["Date"],
+            "y": tmp_data["CumSum"],
+            "type": "lines",
+            "name": time
+        })
+
+    tmp_graph = dcc.Graph(
+                        id=f"{subject_type}",
+                        config={"displayModeBar": False},
+                        figure={
+                            "data": fig_data[:],
+                            "layout": {
+                                "title": {
+                                    "text": f"{subject_type}",
+                                    "x": 0.05,
+                                    "xanchor": "left",
+                                },
+                            },
+                        },
+                    )
+    
+    graph_data.append(tmp_graph)
+        
+
+external_stylesheets = [
+    {
+        "href": "https://fonts.googleapis.com/css2?"
+        "family=Lato:wght@400;700&display=swap",
+        "rel": "stylesheet",
+    },
+]
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.title = "Avocado Analytics: Understand Your Avocados!"
+
+app.layout = html.Div(
+    children=[
+        html.Div(
+            children=[
+                html.P(children="🐢", className="header-emoji"),
+                html.H1(
+                    children="WaniKani Analytics", className="header-title"
+                ),
+                html.P(
+                    children="Analysis of WaniKani data. See how you've progressed in level and how much you've learned",
+                    className="header-description",
+                ),
+            ],
+            className="header",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id="level-data",
+                        config={"displayModeBar": False},
+                        figure={
+                            "data": [
+                                {
+                                    "x": level_data["Level"],
+                                    "y": level_data["Time_seconds"]/(60*60*24),
+                                    "type": "bar",
+                                },
+                            ],
+                            "layout": {
+                                "title": {
+                                    "text": "Time Per Level",
+                                    "x": 0.05,
+                                    "xanchor": "left",
+                                },
+                                "xaxis": {"fixedrange": True, 'title':'Level'},
+                                "yaxis": {"fixedrange": True, 'title':'Days'},
+                                "colorway": ["#17B897"],
+                            },
+                        },
+                    ),
+                    className="card",
+                ),
+                html.Div(
+                    children=graph_data,
+                    className="card",
+                ),
+            ],
+            className="wrapper",
+        ),
+    ]
+)
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
--- wk_progress_review.py
+++ wk_progress_review.py
@@ -34,11 +34,24 @@ def collect_data(endpoint):
     if not check_flag or REFRESH:
         vprint(f"Collecting Data: {endpoint}")
         req = requests.get(f"{WK_URL}/{endpoint}", headers=AUTH_HEADER)
+
         if req.status_code == 200:
+
+            level_data = req.json()
+
+            if level_data['pages']['next_url'] is not None:
+                vprint("Next url is present")
+                tmp_data = requests.get(level_data['pages']['next_url'], headers=AUTH_HEADER)
+                level_data['data'] += tmp_data['data']
+
+                while tmp_data['pages']['next_url'] is not None:
+                    tmp_data = requests.get(level_data['pages']['next_url'], headers=AUTH_HEADER)
+                    level_data['data'] += tmp_data['data']
+
+
             with open(endpoint_path, "w") as f:
                 f.write(json.dumps(req.json(),indent=2))
 
-            level_data = req.json()
         else:
             print(req)
             print("Error collecting data")
@@ -52,7 +65,7 @@ def collect_data(endpoint):
     return level_data
 
 
-def create_level_graph():
+def create_level_data():
     level_data = collect_data("level_progressions")
 
     times = []
@@ -79,27 +92,78 @@ def create_level_graph():
     seconds_diffs = [diff.total_seconds() for diff in diffs]
 
     time_data = {"Level":range(1,61),
-                 "Time":seconds_diffs}
+                 "Time_seconds":seconds_diffs}
     time_df = pd.DataFrame(time_data)
 
-    fig, ax = plot.subplots(figsize=(15,7))
-    bar_plot = sns.barplot(ax=ax, data=time_df, x="Level",y="Time")
+    time_df.to_csv("Data/level_data.csv", index=False)
 
-    highest = int(max(seconds_diffs))
-    day = 60*60*24
-    ticks = range(0,highest,day)
-    tick_labels = range(0,len(ticks))
-    bar_plot.set_yticks(ticks)
-    bar_plot.set_yticklabels(tick_labels)
+    # _, ax = plot.subplots(figsize=(15,7))
+    # bar_plot = sns.barplot(ax=ax, data=time_df, x="Level",y="Time")
 
-    plot.show()
+    # highest = int(max(seconds_diffs))
+    # day = 60*60*24
+    # ticks = range(0,highest,day)
+    # tick_labels = range(0,len(ticks))
+    # bar_plot.set_yticks(ticks)
+    # bar_plot.set_yticklabels(tick_labels)
+
+    # plot.show()
     
-    print(len(diffs))
-    print(total_time)
-    print(avg_time)
+    # print(len(diffs))
+    # print(total_time)
+    # print(avg_time)
 
-    return times
+    # return times
     
+
+def time_collect(assignment_data, key):
+    u_data = []
+    s_data = []
+    p_data = []
+
+    for assignment in assignment_data['data']:
+        if assignment['data']['subject_type'] == key:
+            u_data.append(assignment['data']['unlocked_at'])
+            s_data.append(assignment['data']['started_at'])
+            p_data.append(assignment['data']['passed_at'])
+
+    return u_data[:], s_data[:], p_data[:]
+
+
+
+def collect_progress_data():
+
+    assignment_data = collect_data("assignments")
+
+    df_list = []
+
+    for lesson_type in ("radical", "kanji", "vocabulary"):
+
+        u_data, s_data, p_data = time_collect(assignment_data, lesson_type)
+
+        df_list.append(pd.DataFrame({"Unlock_times":u_data,
+                        "Start_times":s_data,
+                        "Passed_times": p_data}))
+        
+    r_df = df_list[0]
+    k_df = df_list[1]
+    v_df = df_list[2]
+
+    rows_out = []
+
+    for subject_type, df in zip(("radical","kanji","vocab"),(r_df, k_df, v_df)):
+        for column in df.columns:
+            df[column] = pd.to_datetime(df[column])
+
+            tmp_df =r_df.groupby([df[column].dt.date]).count()[column]
+            tmp_series = tmp_df.cumsum()
+
+            for index, row in tmp_series.items():
+                rows_out.append([index,row,column,subject_type])
+
+    out_df = pd.DataFrame(rows_out, columns=['Date','CumSum','Des','Subject'])
+    out_df.to_csv("Data/progress_data.csv",index=False)
+
 
 def main():
 
@@ -111,7 +175,9 @@ def main():
 
     create_store()
 
-    create_level_graph()
+    create_level_data()
+
+    collect_progress_data()
 
 
 if __name__ == "__main__":
