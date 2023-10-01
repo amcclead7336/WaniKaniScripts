d3a29447b3dfd876a2718d95ce55e812d44a335e
amcclead7336 <n7q4c1@u.northwestern.edu>
2023-10-01 17:10:12 -0500

    Add Gantt charts for Radical and Kanji Review times

--- app.py
+++ app.py
@@ -19,6 +19,10 @@ else:
     print("Need to set Environment Var APIKEY")
     sys.exit(1)
 
+# debug = True
+# with open("wk_apikey.txt") as f:
+#     apikey=f.read().strip()
+
 AUTH_HEADER = {"Authorization":f"Bearer {apikey}"}
 
 wk_progress_review.create_dir("Store")
@@ -41,11 +45,14 @@ vlines = wk_progress_review.create_level_lines()
 level_fig = px.bar(x=level_data["Level"], y= level_data["Time_seconds"]/(60*60*24), title="Time Spent per Level", labels={"x":"Level","y":"Time in Days"})
 
 progress_fig = px.line(progress_data, x="Date",y="CumSum",color="Subject", title="Progress per Subject", labels={"x":"Time","y":"Characters"})
+progress_fig.add_trace(go.Line(x=total_series.index, y=total_series.values, name="Total"))
 for vline in vlines:
     progress_fig.add_vline(x=vline, line_color="grey", line_dash="dash")
-progress_fig.add_trace(go.Line(x=total_series.index, y=total_series.values, name="Total"))
 
 
+radical_df, kanji_df = wk_progress_review.create_gantt_data(AUTH_HEADER)
+rad_gantt_fig = px.timeline(radical_df, x_start="Start",x_end="Finish",y="Resource", color="Color")
+kanji_gantt_fig = px.timeline(kanji_df, x_start="Start",x_end="Finish",y="Resource", color="Color")
 
 app.title = "WaniKani Analytics: Understand Your WaniKani!"
 
@@ -84,6 +91,22 @@ app.layout = html.Div(
                         id="progress_fig",
                         config={"displayModeBar": False},
                         figure=progress_fig,
+                    ),
+                    className="card",
+                ),
+                html.Div(
+                    children=dcc.Graph(
+                        id="Radical_Gantt",
+                        config={"displayModeBar": False},
+                        figure=rad_gantt_fig,
+                    ),
+                    className="card",
+                ),
+                html.Div(
+                    children=dcc.Graph(
+                        id="Kanji_Gantt",
+                        config={"displayModeBar": False},
+                        figure=kanji_gantt_fig,
                     ),
                     className="card",
                 ),
